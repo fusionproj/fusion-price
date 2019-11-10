beef77636a71b61b872e4a1be399952b2dbeb5e6
Mohit Tadhiyal <mohtadhi@MacBook-Pro.local>
2019-11-11 00:51:35 +0530

    Updates

---
+++ precache-manifest.a656ebaefca9b8f01a145bb138d5e5be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99358c7de630979f46512ea3b7ec8321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef7900f969010330781f",
+    "url": "/static/css/main.f2914400.chunk.css"
+  },
+  {
+    "revision": "e1a78329b5ccfbce8c16",
+    "url": "/static/js/2.6862b685.chunk.js"
+  },
+  {
+    "revision": "ef7900f969010330781f",
+    "url": "/static/js/main.07f54863.chunk.js"
+  },
+  {
+    "revision": "eeb3ab4ebe53fb4bcb3d",
+    "url": "/static/js/runtime-main.e5b82ed2.js"
+  }
+]);
