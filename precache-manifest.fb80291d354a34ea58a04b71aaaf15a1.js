ca8078582c60e7d41fc78a1278564513e9d143c5
Mohit Tadhiyal <mohtadhi@MacBook-Pro.local>
2019-11-11 12:23:07 +0530

    Updates

---
+++ precache-manifest.fb80291d354a34ea58a04b71aaaf15a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5595abb2d1dc1bc1937c98a26af171f",
+    "url": "/fusion-price/index.html"
+  },
+  {
+    "revision": "31ccfa6ce2003f734ba5",
+    "url": "/fusion-price/static/css/main.f2914400.chunk.css"
+  },
+  {
+    "revision": "ce772c43d4bb57e803e1",
+    "url": "/fusion-price/static/js/2.a189c401.chunk.js"
+  },
+  {
+    "revision": "31ccfa6ce2003f734ba5",
+    "url": "/fusion-price/static/js/main.6eadc67a.chunk.js"
+  },
+  {
+    "revision": "c60ae13d86203bfff04d",
+    "url": "/fusion-price/static/js/runtime-main.7e80c8d8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a656ebaefca9b8f01a145bb138d5e5be.js"
+  "/fusion-price/precache-manifest.fb80291d354a34ea58a04b71aaaf15a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fusion-price/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
